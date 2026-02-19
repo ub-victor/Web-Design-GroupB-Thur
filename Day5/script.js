fd9c5db2163bc331bf9989d270996291f16fa32c
ub-victor <victoireushindi371@gmail.com>
2026-02-19 10:46:19 +0200

    JavaScript learning

--- Day5/script.js
+++ Day5/script.js
@@ -1,17 +1,4 @@
-calculate (()=>{
-    let a = prompt("Enter value of A");
-    let b = prompt("Enter value of B");
-
-    // Convert inputs to numbers
-    const A = Number(a);
-    const B = Number(b);
-
-    // Optional: validate inputs
-    if (Number.isNaN(A) || Number.isNaN(B)) {
-      document.write("Invalid input. Please enter numbers for A and B.");
-    } else {
-      const c = A * B;
-      document.write("Result: " + c);
-    }
-});
-
+function Calculate(){
+  let A = prompt("Enter the value of A");
+  let B = prompt("Enter the value of B");
+}
