434a05759ba3082e752fdf22db639723f076c602
ub-victor <victoireushindi371@gmail.com>
2026-02-19 09:57:20 +0200

    JavaScript learning

--- Day5/script.js
+++ Day5/script.js
@@ -1,9 +1,6 @@
-prompt("Enter your name to continue");
-        alert('Click on "Ok" to continue')
-        document.write("We are displauying this statement");
+let a = prompt ("Enter value of A");
+let b = prompt("Enter value of B");
 
-        let a=20;
-        let b = 30;
-        let c = a*b;
-        console.log(c);
-        document.write(c);
+let c = a+b;
+
+Document.write(c);
