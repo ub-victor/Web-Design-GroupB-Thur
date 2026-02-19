f3f435ad565edaf698dc817f4bd3ed8e490923c3
ub-victor <victoireushindi371@gmail.com>
2026-02-19 10:49:30 +0200

    JavaScript learning

--- Day5/script.js
+++ Day5/script.js
@@ -1,4 +1,11 @@
 function Calculate(){
   let A = prompt("Enter the value of A");
   let B = prompt("Enter the value of B");
+
+  let Prod= A*B;
+  document.write(Prod)
+
+  function Change(){
+    document.getElementById('this').innerHTML="Display This"
+  }
 }
