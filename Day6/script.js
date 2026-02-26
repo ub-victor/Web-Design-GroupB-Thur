cd4feafca9dd6a49abee0e283f33c3dd6e46af94
ub-victor <victoireushindi371@gmail.com>
2026-02-26 10:33:15 +0200

    Day6

--- Day6/script.js
+++ Day6/script.js
@@ -1,8 +1,20 @@
 function Monthly(){
+    let disply = document.getElementById('di');
     let Gross = parseInt(document.getElementById('gs').value);
     let Transport = parseInt(document.getElementById('ta').value);
     let Insurance = parseInt(document.getElementById('hi').value);
     let Tax = parseInt(document.getElementById('tx').value);
     let MSalary = Gross + Transport - Insurance - Tax;
-    document.write(MSalary)
+    disply.innerHTML = MSalary;
+}
+
+function Annual(){
+    let disply = document.getElementById('di');
+    let Gross = parseInt(document.getElementById('gs').value);
+    let Transport = parseInt(document.getElementById('ta').value);
+    let Insurance = parseInt(document.getElementById('hi').value);
+    let Tax = parseInt(document.getElementById('tx').value);
+    let MSalary = Gross + Transport - Insurance - Tax;
+    let ASalary = MSalary * 12;
+    disply.innerHTML = ASalary;
 }
